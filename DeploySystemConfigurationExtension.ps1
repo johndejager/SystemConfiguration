e979de1880b679687d8b337acdf42a5e006dd7b6
John de Jager <johndejager@gmail.com>
2018-07-05 12:44:34 +0200

    Created deployment method by Powershell

---
+++ DeploySystemConfigurationExtension.ps1
@@ -0,0 +1,16 @@
+﻿Login-AzureRmAccount
+
+$Subscription = Get-AzureRmSubscription | Out-GridView -Title "Select Subscription" -OutputMode Single
+Select-AzureRmSubscription -SubscriptionId $Subscription.Id
+
+$VM = Get-AzureRmVM | Out-GridView -Title "Select VM" -OutputMode Single
+
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $VM.ResourceGroupName`
+    -VMName $VM.Name `
+    -Location $VM.Location `
+    -FileUri "https://raw.githubusercontent.com/johndejager/SystemConfiguration/master/SystemConfiguration.ps1","https://github.com/johndejager/SystemConfiguration/blob/master/SystemConfiguration.zip" `
+    -Run 'SystemConfiguration.ps1' `
+    -Name SystemConfigurationExtension
+
+#### Output log -  C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension
+#### Download dir - C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.*\Downloads\<n>
